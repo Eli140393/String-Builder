1545a67cca6eaedb8ef87a8a57f6cf390926010a
Eliezer Defilicibus <eli.defilicibus@gmail.com>
2020-06-17 20:31:45 -0300

    String Builder

---
+++ StringBuilder/Entities/Comment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CourseString.Entities
+{
+    class Comment
+    {
+        public string Text { get; set; }
+        public Comment()
+        {
+
+        }
+        public Comment(string text)
+        {
+            Text = text;
+        }
+
+
+    }
+}
